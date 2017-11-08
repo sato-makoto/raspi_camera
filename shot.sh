8feb44c6c8751773d40f895eacbb3736e03f6047
Sato Makoto <makoto@kuzuore.com>
2017-11-09 08:49:47 +0900

    add simple snap script

---
+++ shot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# still snap
+
+EXE=picture
+PICT_W=800
+PICT_H=600
+  
+DIR=`date "+%Y/%m%d"`
+
+SAVE=$HOME/photo/${DIR}
+
+if [ ! -d $SAVE ]; then
+  mkdir -p $SAVE
+fi
+
+if [ "$?" != "0" ]; then
+  echo Cannot make dir.
+  exit 1
+fi
+
+FILENAME=`date "+%H-%M-%S.png"`
+
+raspistill \
+  -t 1 \
+  -e png  \
+  -w $PICT_W -h $PICT_H \
+  -o ${SAVE}/${FILENAME} 
+
+echo ${SAVE}/${FILENAME}
+
+
+# fbi -a -t 3 -1 -T 1 -d /dev/fb0  ${SAVE}/${FILENAME}
+display  ${SAVE}/${FILENAME}
+
